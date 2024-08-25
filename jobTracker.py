31ee2275bc2d4fc3e2df7f2d220c6cf9ebe5590a
Aravind Sri Sai Kisore <62432516+aravindcmd@users.noreply.github.com>
2024-08-25 15:54:31 +0200

    Update jobTracker.py

--- jobTracker.py
+++ jobTracker.py
@@ -1,7 +1,7 @@
 val = input('paste the url \n')
 flag = False
 
-with open('/home/aravind/jobFile.txt', 'r') as file:
+with open('<ENTER FILE PATH>/jobFile.txt', 'r') as file:
 	for line in file:
 		if val in line:
 			flag = True
@@ -11,10 +11,7 @@ if(flag == True):
 	print("MATCH FOUND")
 else:
 	print("NOT applied")
-	with open('/home/aravind/jobFile.txt', 'a') as file:
+	with open('<ENTER FILE PATH>/jobFile.txt', 'a') as file:
 		file.write(val+'\n')
 	print('added to the list')
 
-        	# print(f"Found the string '{target_string}' in the");
-
-# print(html_as_string.count("profile-container"))
